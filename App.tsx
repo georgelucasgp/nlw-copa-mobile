2a5cfbfa767036c9906fee121c10c0988928c433
georgelucasgp <djgeorgelucas@hotmail.com>
2022-11-05 15:50:11 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { StatusBar } from "expo-status-bar";
+import { Center, NativeBaseProvider, Text } from "native-base";
+import { THEME } from "./src/styles/theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="gray.900">
+        <Text color="white" fontSize={24}>
+          Hello Word2!!
+        </Text>
+        <StatusBar style="auto" />
+      </Center>
+    </NativeBaseProvider>
+  );
+}
